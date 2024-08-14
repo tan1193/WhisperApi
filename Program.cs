cdde0b786c619997f28ccccee1bca72cde461d74
Tan Hoang <tan.hoang@thirdsight.net>
2024-08-14 20:12:04 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using OpenAI.Audio;
+using System.ClientModel;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapPost("/audiotranscribe", async () =>
+{
+    openApiKey = "";
+    var model = "whisper-1";
+    // options for audio transcription
+    var audioTranscriptionOptions = new AudioTranscriptionOptions
+    {
+        ResponseFormat = AudioTranscriptionFormat.Srt,
+
+    };
+    var audioClient = new AudioClient(model, openApiKey);
+
+    var response = await audioClient.TranscribeAudioAsync("audio.mp3", audioTranscriptionOptions);
+    return response.Value.Text;
+})
+.WithName("AudioTranscribe")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
