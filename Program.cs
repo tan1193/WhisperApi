3c947f7ebd769fe039abfc66df9be9654052846b
Tan Hoang <tan.hoang@thirdsight.net>
2024-08-15 15:14:22 +0700

    remove redundant code

--- Program.cs
+++ Program.cs
@@ -20,26 +20,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.MapPost("/audiotranscribe", async () =>
 {
     var openApiKey = "";
@@ -62,7 +42,7 @@ app.MapPost("/audiotranslation", async () =>
 {
     var openApiKey = "";
     var model = "whisper-1";
-    // options for audio transcription
+    
     var audioTranscriptionOptions = new AudioTranslationOptions
     {
         ResponseFormat = AudioTranslationFormat.Srt,
@@ -77,8 +57,3 @@ app.MapPost("/audiotranslation", async () =>
 .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
